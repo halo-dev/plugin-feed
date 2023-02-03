e69f28913802b23880557a044b563c38f5187632
longjuan <769022681@qq.com>
2023-02-03 16:35:36 +0800

    feat: RSS feeds by author, category and all posts (#3)

--- src/main/java/run/halo/feed/FeedPlugin.java
+++ src/main/java/run/halo/feed/FeedPlugin.java
@@ -2,20 +2,13 @@ package run.halo.feed;
 
 import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
-import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 
-/**
- * @author ryanwang
- * @since 2.0.0
- */
 @Component
 public class FeedPlugin extends BasePlugin {
-    private final SchemeManager schemeManager;
 
     public FeedPlugin(PluginWrapper wrapper) {
         super(wrapper);
-        this.schemeManager = getApplicationContext().getBean(SchemeManager.class);
     }
 
     @Override
