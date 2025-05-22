00ecfcd768dab682883355853922ae64e4739144
Ryan Wang <i@ryanc.cc>
2025-05-22 11:40:56 +0800

    Refactor data list using VEntityContainer
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/src/components/SourcesTab.vue
+++ ui/src/components/SourcesTab.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { axiosInstance } from "@halo-dev/api-client";
-import { IconInformation, VEntity, VEntityField, VLoading } from "@halo-dev/components";
+import { IconInformation, VEntity, VEntityContainer, VEntityField, VLoading } from "@halo-dev/components";
 import { useQuery } from "@tanstack/vue-query";
 
 interface Source {
@@ -23,22 +23,17 @@ const { data, isLoading } = useQuery({
   <div class="m-4">
     <VLoading v-if="isLoading" />
     <div v-else class="overflow-hidden border rounded">
-      <ul class="box-border h-full w-full divide-y divide-gray-100" role="list">
-        <li v-for="source in data" :key="source.pattern">
-          <VEntity class="group">
-            <template #start>
-              <VEntityField :title="source.displayName" :description="source.pattern">
-                <template #extra>
-                  <IconInformation
-                    v-tooltip="source.description"
-                    class="hidden size-4 text-gray-600 group-hover:block"
-                  />
-                </template>
-              </VEntityField>
-            </template>
-          </VEntity>
-        </li>
-      </ul>
+      <VEntityContainer>
+        <VEntity v-for="source in data" :key="source.pattern" class="group">
+          <template #start>
+            <VEntityField :title="source.displayName" :description="source.pattern">
+              <template #extra>
+                <IconInformation v-tooltip="source.description" class="hidden size-4 text-gray-600 group-hover:block" />
+              </template>
+            </VEntityField>
+          </template>
+        </VEntity>
+      </VEntityContainer>
     </div>
   </div>
 </template>
